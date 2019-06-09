f9ee596c63484fa29a3aa656b1fee1744336d487
paolofruci <paolo.fruci@gmail.com>
2019-06-09 19:35:06 +0200

    add db connection test

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+  $dbServer = "172.26.49.16";
+  $dbUser = "vmware";
+  $dbPassword = "Password1";
+  $dbName = "vmware";
+  $db  = new mysqli($dbServer, $dbUser, $dbPassword, $dbName);
+    if ($db->connect_errno) { 
+        echo "Connection Failed";
 
 echo "<h1> la mia pagina di test versione 2 </h1>";
 echo "<h2> eseguo test sui commit e rebuild di openshift</h2>";
