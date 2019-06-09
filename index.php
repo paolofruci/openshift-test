cddbf3d74b146c2b593f228c77a7dc469bc4bf9f
paolofruci <paolo.fruci@gmail.com>
2019-06-09 19:44:27 +0200

    fix errors

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   $db  = new mysqli($dbServer, $dbUser, $dbPassword, $dbName);
     if ($db->connect_errno) { 
         echo "Connection Failed";
-
+    }
 echo "<h1> la mia pagina di test versione 2 </h1>";
 echo "<h2> eseguo test sui commit e rebuild di openshift</h2>";
 echo "";
